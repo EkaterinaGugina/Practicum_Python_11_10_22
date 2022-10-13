5328005ac5aa09eb9b893faabf4dc254895a396c
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-10-14 02:53:03 +0500

    update hw_07_10

--- HomeWork_07_10_22/Ex1_confi_bot.py
+++ HomeWork_07_10_22/Ex1_confi_bot.py
@@ -1,46 +1,56 @@
 # вариант человек против бота без интеллекта:
 from random import randint
 
-# def input_dat(name):
-#     x = int(input(f"{name}, введите сколько конфет возьмете (число от 1 до 28): "))
-#     while x < 1 or x > 28:
-#         x = int(input(f"{name}, такое количество конфет брать нельзя! \
-#             \n Введите количество конфет (число от 1 до 28): "))
-#     return x
+def input_dat(name):
+    x = int(input(f"{name}, введите сколько конфет возьмете (число от 1 до 28): "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, такое количество конфет брать нельзя! \
+            \n Введите количество конфет (число от 1 до 28): "))
+    return x
 
 
-# def res_out_print(name, k, count, value):
-#     print(f"Ход: {name}, взял {k}, теперь у него {count}. В кучке сталось {value} конфет.")
+def res_out_print(name, k, count, value):
+    print(f"Ход: {name}, взял {k}, теперь у него {count}. В кучке сталось {value} конфет.")
 
-# name1, name2 = "Человек", "БотA"
-# value = 2021
-# flag = randint(0,2)  # очередность: 0 - человек, 1 - БотА
-# if flag:
-#     print(f"Первый ходит {name1}")
-# else:
-#     print(f"Первый ходит {name2}")
+name1, name2 = "Человек", "БотA"
+value = 2021
+flag = randint(0,2)  # очередность: 0 - человек, 1 - БотА
+if flag:
+    print(f"Первый ходит {name1}")
+else:
+    print(f"Первый ходит {name2}")
 
-# count1 = 0 
-# count2 = 0
+k = int(121 % 28)
+count1 = 0 
+count2 = 0
+if flag:                   # Первый ход - случайное число 
+        print(f'{name1}, если хочешь победить, бери {k} штук в первый раз, а потом смотри подсказки на экране')
+        count1 += k
+        value -= k
+        flag = False
+        res_out_print(name1, k, count1, value)
+else:
+    flag = False
+    res_out_print(name2, k, count2, value)
 
-# while value > 28:
-#     if flag:
-#         k = input_dat(name1)
-#         count1 += k
-#         value -= k
-#         flag = False
-#         res_out_print(name1, k, count1, value)
-#     else:
-#         k = randint(1,29)
-#         count2 += k
-#         value -= k
-#         flag = True
-#         res_out_print(name2, k, count2, value)
+while value > 28:
+    if flag:
+        k = input_dat(name1)
+        count1 += k
+        value -= k
+        flag = False
+        res_out_print(name1, k, count1, value)
+    else:
+        k = randint(1,29)
+        count2 += k
+        value -= k
+        flag = True
+        res_out_print(name2, k, count2, value)
 
-# if flag:
-#     print(f"Выиграл {name1}")
-# else:
-#     print(f"Выиграл {name2}")
+if flag:
+    print(f"Выиграл {name1}")
+else:
+    print(f"Выиграл {name2}")
     
     
     
@@ -58,7 +68,7 @@ from random import randint
 
 # name1, name2 = "Первый игрок", "Второй игрок"
 # value = 2021
-# flag = randint(0,2)             # очередность: 0 - 1человек, 1 - 2человек
+# flag = randint(0,2)             # очередность: 0 - 2человек, 1 - 1человек
 # if flag:
 #     print(f"Первый ходит {name1}")
 # else:
@@ -101,7 +111,7 @@ from random import randint
 
 # name1, name2 = "Человек", "БотВ"
 # value = 121
-# flag = randint(0,2)             # очередность: 0 - 1человек, 1 - 2человек
+# flag = randint(0,2)             # очередность: 0 - БотВ, 1 - Человек
 # if flag:
 #     print(f"Первый ходит {name1}")
 # else:
@@ -117,7 +127,7 @@ from random import randint
 #         res_out_print(name1, k, count1, value)
 # else:
 #     flag = False
-#     res_out_print(name1, k, count2, value)
+#     res_out_print(name2, k, count2, value)
 # while value > 28:
 #     if flag:
 #         k = input_dat(name1)
