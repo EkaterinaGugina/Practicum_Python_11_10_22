e4209b19aedcfeb591be8043362dbf2c3f2a3fc1
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-10-18 21:40:42 +0500

    add Ex2

--- HomeWork_11_10_22/Ex2_simpl_div_N_filter.py
+++ HomeWork_11_10_22/Ex2_simpl_div_N_filter.py
@@ -1,6 +1,4 @@
-# Ex2_simpl_div_N_filter. Задайте натуральное число N. Напишите программу, 
-# которая составит список простых множителей числа N.
-
-n = int(input('Введите натуральное число N = '))
-spisok_simpl_div_N = [x for x in range(col) if n % x == 0  and not x % spisok_simpl_div_N[i-1]    lambda n: n//x]
-print(f'Это все простые делители числа {n}: {spisok_simpl_div_N}')
+# Ex2_sum_num_real_filter. Найти сумму цифр вещественного числа.
+str_r = "100.00087"
+sum_num_real = sum([int(str_r[i]) for i in range(len(str_r)) if str_r[i] not in {',','.'}])         
+print(f'{str_r} ---> сумма цифр этого числа равна {sum_num_real}')
